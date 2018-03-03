c3b447e777d3fa518722d1b6ef3f01e08ac87a25
Jorge Andrade <jorandradefig@gmail.com>
2018-03-03 02:50:26 -0600

    Add title slide

--- slides/presentation/index.js
+++ slides/presentation/index.js
@@ -11,7 +11,8 @@ import {
   List,
   Quote,
   Slide,
-  Text
+  Text,
+  Link
 } from "spectacle";
 
 // Import theme
@@ -33,32 +34,22 @@ const theme = createTheme({
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck transition={["slide"]} transitionDuration={500} theme={theme}>
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={1} fit lineHeight={1} textColor="secondary">
+            Personas Desaparecidas
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
+          <Link
+            href="https://personasdesaparecidas.org.mx"
+            target="_blank"
+            margin="10px 0 0"
+            textColor="tertiary"
+            size={1}
+            fit
+            bold
+          >
+            https://personasdesaparecidas.org.mx
+          </Link>
         </Slide>
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <BlockQuote>
